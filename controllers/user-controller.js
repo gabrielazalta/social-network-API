fa2871eb0d062d8cf44456883bd7382577a1b519
Gabriela Zalta <gzalta96@gmail.com>
2021-03-05 11:17:51 -0500

    thought-controller complete

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,15 +15,7 @@ const userController = {
     //GET /api/users/:id
     getUserById( { params }, res) {
         User.findOne({ _id: params.id })
-        .populate({ 
-                path: 'thoughts',
-                select: -__v
-            },
-            { 
-                path: 'friends',
-                select: -__v
-            }
-            )
+        .populate({ path: 'thoughts', select: -__v }, { path: 'friends', select: -__v })
         .select('-__v')
         .then((dbUserData) => {
             if (!dbUserData) {

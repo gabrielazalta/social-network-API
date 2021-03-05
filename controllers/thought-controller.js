b58aaf2ee8d3b09f0ab84438fba38fa4b34bf22b
Gabriela Zalta <gzalta96@gmail.com>
2021-03-05 10:33:05 -0500

    user-controller api routes done

---
+++ controllers/thought-controller.js
@@ -0,0 +1,20 @@
+const { User, Thought, Reaction } = require('../models');
+
+const thoughtController = {
+    // GET /api/thoughts
+
+    //GET /api/thoughts/:id
+
+    //POST /api/thoughts/:id
+
+    //PUT /api/thoughts/:id
+
+    //DELETE /api/thoughts/:id
+
+    //POST /api/thoughts/:thoughtId/reactions
+
+    //DELETE /api/thoughts/:thoughtId/reactions
+
+}
+
+model.exports = thoughtController;
